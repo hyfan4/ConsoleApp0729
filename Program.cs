66b0f5d69b5b27a69e641dcd3d0f8dc6fc2f7baa
hyfan4 <lisa100177@gmail.com>
2025-07-29 10:38:15 +0800

    v1

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using static System.Console;
+namespace ConsoleApp729
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 0;
+            int b = 1;
+            int result = 0;
+            Write($"{a} {b} ");
+
+            for (int i = 2; i < 10; i++)
+            {
+                result = a + b;
+                a = b;
+                b = result;
+
+                Write($"{result} ");
+            }
+
+        }
+    }
+}
+
