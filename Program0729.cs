ca9521721e2be604cd0969b877f650f8b9d1e1a5
hyfan4 <lisa100177@gmail.com>
2025-07-29 10:01:15 +0800

    v0

---
+++ Program0729.cs
@@ -0,0 +1,22 @@
+﻿using static System.Console;
+namespace ConsoleApp0729
+    //
+{
+    internal class Program
+    {
+        public static int f(int n)
+        {
+            if (n == 1)
+            {
+                return 1;
+            }else{
+                return n * f(n - 1);
+            }
+        }
+        static void Main(string[] args)
+        {
+            int x = f(5);
+            WriteLine(x);
+        }
+    }
+}
